793bb8dc45250b9fa4e5ec7967d5869905899e75
poetchess <ilvbaicai@126.com>
2017-09-01 16:17:27 +0800

    Enhancement, adding exception handling

--- scrapetest.py
+++ scrapetest.py
@@ -1,8 +1,23 @@
 from urllib.request import urlopen
+from urllib.error import HTTPError
 from bs4 import BeautifulSoup
 
-html = urlopen('http://pythonscraping.com/pages/page1.html')
-bsObj = BeautifulSoup(html.read(), 'html.parser')
-print(bsObj.h1)
+def getTitle(url):
+    try:
+        html = urlopen(url)
+    except HTTPError as e:
+        return None
+    try:
+        bsObj = BeautifulSoup(html.read(), 'html.parser')
+        title = bsObj.body.h1
+    except AttributeError as e:
+        return None
+    return title
+
+title = getTitle('http://pythonscraping.com/pages/page1.html')
+if title is None:
+    print('Title could not be found')
+else:
+    print(title)
 
 
